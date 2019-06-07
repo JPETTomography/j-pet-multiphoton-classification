c6d4263dfb9ce1c0335812bff7ab0f0074c53b53
K4liber <janbielecki94@gmail.com>
2019-06-07 11:58:03 +0200

    TranformingData corrections

--- TranformingData/transformingDataExample.py
+++ TranformingData/transformingDataExample.py
@@ -1,41 +1,9 @@
 #!/usr/bin/env python3.6
 
-import dask.dataframe as dd
 import pandas as pd
 import sys
 import pickle
 import matplotlib.pyplot as plt
-
-def dataFrameNames():
-    return [
-        "x1",     # 1 gamma detected x position [cm]
-        "y1",     # 1 gamma detected y position [cm]
-        "z1",     # 1 gamma detected z position [cm]
-        "t1",     # 1 gamma detection time [ps]
-        "x2",     # 2 gamma detected x position [cm]
-        "y2",     # 2 gamma detected y position [cm]
-        "z2",     # 2 gamma detected z position [cm]
-        "t2",     # 2 gamma detection time [ps]
-        "vol1",   # 1 gamma volume ID
-        "vol2",   # 2 gamma volume ID
-        "e1",     # 1 gamma energy loss during detection [keV]
-        "e2",     # 2 gamma energy loss during detection [keV]
-        "class",  # Type of coincidence(1-true, 2-phantom-scattered, 3-detector-scattered, 4-accidental)
-        "sX1",    # 1 gamma emission x position [cm]
-        "sY1",    # 1 gamma emission y position [cm]
-        "sZ1",     # 1 gamma emission z position [cm]
-        "dt",     # Detection times difference
-        "rX1",    # Reconstruction point - X cord
-        "rY1",    # Reconstruction point - Y cord
-        "rZ1",    # Reconstruction point - Z cord
-        "rError", # Difference beetwen source point and recontructed point
-        "volD",   # Volumes indexes difference
-        "lorL",   # LOR length
-        "deg3D",  # Angle beetwen lines (in XYZ geometry) connecting detection points with the center of detector
-        "deg2D",  # Angle beetwen lines (in XY geometry) connecting detection points with the center of detector
-        "rL",     # Distance beetween reconstructed point and the center of detector
-        "eSum"    # Sum of the detecions energies
-    ]
 
 pathToFile = 'NEMA_IQ_384str_N0_1000_COINCIDENCES_PREPARED_part00'
 
